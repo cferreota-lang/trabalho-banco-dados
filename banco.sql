056e831850ff2d5c1802ace019d805f02378969e
cferreota-lang <cferreota@gmail.com>
2026-09-22 09:50:07 -0300

    Criação da tabela Alunos e inserção de registros

---
+++ banco.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Alunos
+ Id INT AUTO_INCREMENT,
+    Nome VARCHAR(60) NOT NULL,
+    Curso VARCHAR(60) NULL,
+    PRIMARY KEY (Id)
+INSERT INTO Alunos (Nome, curso)
+INSERT INTO Alunos (Nome, Curso)
