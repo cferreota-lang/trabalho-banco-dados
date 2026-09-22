b8558bbee4bf542da275cf51380dadb429bf7a90
cferreota-lang <cferreota@gmail.com>
2026-09-22 09:59:21 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,7 +1,9 @@
-CREATE TABLE Alunos
- Id INT AUTO_INCREMENT,
+CREATE TABLE Alunos (
+    Id INT AUTO_INCREMENT,
     Nome VARCHAR(60) NOT NULL,
     Curso VARCHAR(60) NULL,
     PRIMARY KEY (Id)
-INSERT INTO Alunos (Nome, curso)
-INSERT INTO Alunos (Nome, Curso)
+);
+
+INSERT INTO Alunos (Nome, Curso) VALUES ('Maria Silva', 'Análise de Sistemas');
+INSERT INTO Alunos (Nome, Curso) VALUES ('João Santos', 'Engenharia de Software');
